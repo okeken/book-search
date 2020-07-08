517ce79c5e59f3765dae920c105d2a686f3a9406
okekenny <okekehinde@gmail.com>
2020-07-08 12:02:36 +0100

    More Info Showing- Not complete yet

--- src/App.js
+++ src/App.js
@@ -29,8 +29,9 @@ function App() {
   const [isError, setIsError] = useState(false);
   const [countMe, setCountMe] = useState(0);
   const [volumeID, setVolumeID] = useState('L4HOcQAACAAJ');
-  const [volumeData, setVolumeData] = useState('');
+  const [volumeData, setVolumeData] = useState({});
   const [isLoadingB, setIsLoadingB] = useState(false);
+  const [n, setN] = useState();
 
   let fetchVolumeInfo = async () => {
     setIsLoadingB(true);
@@ -39,12 +40,6 @@ function App() {
         `https://www.googleapis.com/books/v1/volumes/${volumeID}?key=${process.env.REACT_APP_KEY}`
       );
       setVolumeData(volResult.data);
-      console.log('vol fetch', volumeData);
-      console.log('volume id :', volumeID);
-      console.log(
-        'url: ',
-        `https://www.googleapis.com/books/v1/volumes/${volumeID}?key=${process.env.REACT_APP_KEY}`
-      );
     } catch (e) {
       console.log('error occured', e);
     }
@@ -77,9 +72,12 @@ function App() {
   ) : null;
 
   let btnStyle = !showDet ? { margin: '1rem auto' } : { margin: '1rem 0rem' };
+  let arr = [];
+  let newArr = [];
+
   return (
     <>
-      <div className={resultShow == 0 ? 'dark-bg' : null}>
+      <div className={resultShow === 0 ? 'dark-bg' : null}>
         <div className={`my-form ${!showDet ? 'not-loading' : 'loaded-res'}`}>
           <form
             onSubmit={(e) => {
@@ -127,6 +125,27 @@ function App() {
             )}
           </form>
         </div>
+        {arr.push({
+          id: data.items
+            .map((items) => items.id)
+            .join(',')
+            .split(','),
+          author: data.items
+            .map((items) => items.volumeInfo.authors)
+            .join(',')
+            .split(','),
+          title: data.items
+            .map((items) => items.volumeInfo.title)
+            .join(',')
+            .split(','),
+          description: data.items.map((items) => items.volumeInfo.description),
+
+          category: data.items
+            .map((items) => items.volumeInfo.title)
+            .join(',')
+            .split(','),
+        })}
+        {newArr.push(arr)}
         {!isError & !isLoading && (
           <>
             <div className='book-search-section'>
@@ -228,20 +247,55 @@ function App() {
                     </div>
 
                     <Button
-                      className={`${volumeID === items.id ? 'test-css' : null}`}
+                      className='btn-results'
                       type='submit'
                       color='primary is-light'
                       value={items.id}
                       onClick={(e) => {
                         setVolumeID(e.target.value);
                         fetchVolumeInfo();
-                        console.log('body', volumeID);
+                        let a = arr[0];
+                        let c = a.id.findIndex((i) => i === items.id);
+                        setN(c);
                       }}
                     >
                       {' '}
                       Get More Info
                     </Button>
                     <hr className='hr-book-results' />
+
+                    <div className='more-info-div'>
+                      <div
+                        className={`modal ${
+                          volumeID === items.id ? 'is-active' : null
+                        }`}
+                      >
+                        <div class='modal-background'></div>
+                        <div class='modal-card'>
+                          <header className='modal-card-head'>
+                            <p className='modal-card-title'>
+                              <p>{arr[0].title[n]} </p>
+                            </p>
+                            <button
+                              onClick={() => {
+                                setVolumeID('');
+                              }}
+                              className='delete'
+                              aria-label='close'
+                            ></button>
+                          </header>
+                          <section className='modal-card-body'>
+                            <p>{arr[0].description[n]} </p>
+                          </section>
+                          <footer className='modal-card-foot'>
+                            <button className='button is-success'>
+                              Save changes
+                            </button>
+                            <button className='button'>Cancel</button>
+                          </footer>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </>
               ))}
